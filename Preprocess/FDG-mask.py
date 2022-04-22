c73bbc56ab25269d22cfe5eb66379eab93ffa235
Ferri-00 <eli.ferri.2000@gmail.com>
2022-04-22 09:35:27 +0200

    improved masking and functioning file for KMeans algorithm

---
+++ Preprocess/FDG-mask.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import time
+
+def group_data(file_number):    
+    for v in file_number:
+        file_name = f".\File\FrontendFile\storm-frontend-202003{v}-mask.txt"
+#         file_name = f".\File\FrontendFile\storm-frontend-202003{v}-mask.txt"
+
+#         frontend = pd.read_csv(file_name, nrows = 1e4)
+        frontend = pd.read_csv(file_name)
+        print("reading ", file_name)
+        
+        frontend = frontend.sort_values(by = ['id'])
+        frontend = frontend.set_index('id')
+
+        new_frontend = pd.DataFrame(columns = ['PID', 'Level', 'date_time_start', 'date_time_end', 'message'], index = set(frontend.index))
+
+        for i in set(frontend.index):
+            PID = frontend['PID'][i]
+            message = list(frontend.loc[i]['message'])
+
+
+            if type(frontend.loc[i]) == pd.core.series.Series:
+                new_frontend.loc[i]['PID'] = PID
+                new_frontend.loc[i]['message'] = message
+
+            else:
+                frontend.loc[i] = frontend.loc[i].sort_values(by = ['time'])
+
+                if len(set(PID)) == 1:
+                    new_frontend.loc[i]['PID'] = PID[0]
+                else:
+                    print(f"Different threads associated to the same ID {i}")
+                    break    
+
+                new_frontend.loc[i]['message'] = message[0]
+                for msg in message[1:]:
+                    new_frontend.loc[i]['message'] += ' ' + msg
+
+            Level = set(frontend['Level'][i])
+            new_frontend.loc[i]['Level'] = str()
+            for l in Level:
+                new_frontend.loc[i]['Level'] += l
+
+            date_time = list(frontend.loc[i]['date'] + ' ' + frontend.loc[i]['time'])
+            new_frontend.loc[i]['date_time_start'] = date_time[0]
+            new_frontend.loc[i]['date_time_end'] = date_time[-1]
+                
+        print(f"saving storm-frontend-202003{v}-group.txt")
+        new_frontend.to_csv(f"./File/FrontendFileGroup/storm-frontend-202003{v}-mask-group.txt")
+
+print("Starting grouping logs")
+
+t0 = time.time()
+
+# group_data(["07"])
+group_data(["08","09","10","11","12", "13"])
+
+print(f"done in {int((time.time()-t0)/60)} minutes and {((time.time()-t0)%60)} seconds")
--- Preprocess/Masking.py
+++ Preprocess/Masking.py
@@ -11,14 +11,14 @@ def masking_frontend_data(file_number):
         file_name = f"./File/FrontendFile/storm-frontend-202003{v}.txt"
         print("reading ", file_name)
     
-        data = pd.read_csv(file_name, nrows=5e4)
+        data = pd.read_csv(file_name)
         f = data.message
 
-        specific_substitute = 'IP'
-
         for i in f.index:
-            f[i] = re.sub(ipv6_address, specific_substitute, f[i])
-            f[i] = re.sub(ipv4_address, specific_substitute, f[i])
+            f[i] = re.sub(ipv6_address, '<IP>', f[i])
+            f[i] = re.sub(ipv4_address, '<IP>', f[i])
+            f[i] = re.sub('(\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12})', '<TOKEN>', f[i])    
+
 
         data.message = f
         
@@ -26,7 +26,7 @@ def masking_frontend_data(file_number):
         data.to_csv(f"./File/FrontendFile/storm-frontend-202003{v}-mask.txt", index=False)
 #         data.to_csv(f"./Tesi-Operational-Intelligence/storm-frontend-202003{v}-mask.txt", index=False)
         
-t0= time.time()
+t0 = time.time()
 
 masking_frontend_data(["07"])
 masking_frontend_data(["08","09","10","11","12", "13"])
