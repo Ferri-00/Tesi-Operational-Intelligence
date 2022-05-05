ab43293b861be636e2741a3239028599605da80d
Ferri-00 <eli.ferri.2000@gmail.com>
2022-05-05 16:59:19 +0200

    abc

---
+++ Preprocess/ErrorDivision.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import time
+
+import re
+
+def division_errors(file_number):
+    for v in file_number:
+        file_name = f"./File/FrontendFileGroup/storm-frontend-202003{v}-mask-group.txt"
+        # file_name = f"../File/FrontendFileGroup/storm-frontend-202003{v}-group.txt"
+        
+        print('reading', file_name)
+        logs = pd.read_csv(file_name, index_col=0)
+        errors = pd.DataFrame(columns=logs.columns)
+        
+        for idx, msg in zip(logs.index, logs.message):
+            resultE = re.search('error', msg.lower())
+            resultF = re.search('failure', msg.lower())
+            resultP = re.search('problem', msg.lower())
+            if resultE!=None or resultF!=None or resultP!=None:
+                errors.loc[idx] = logs.loc[idx]
+                logs = logs.drop(idx, axis=0)
+
+        logs.to_csv(f"./File/FrontendFileErr/storm-frontend-202003{v}-msg.txt", index=False)
+        errors.to_csv(f"./File/FrontendFileErr/storm-frontend-202003{v}-err.txt", index=False)
+        
+print("Starting division of files")
+
+t0=time.time()
+
+division_errors(["07"])
+division_errors(["08","09","10","11","12", "13"])
+
+print(f"done in {int((time.time()-t0)/60)} minutes and {((time.time()-t0)%60)} seconds")
