19df2d9e6533e9c3583a47b56cc442107b56e267
Ferri-00 <eli.ferri.2000@gmail.com>
2022-05-13 11:02:56 +0200

    old file Untitled has been renamed

---
+++ OptimalNCluster.py
@@ -0,0 +1,111 @@
+# import libraries
+from __future__ import print_function
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+
+from collections import Counter, defaultdict
+
+import time
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn import metrics
+from sklearn.metrics import silhouette_score
+
+
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
+import logging
+from optparse import OptionParser
+import sys
+from time import time
+
+# Display all cell outputs
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = 'all'
+
+from IPython import get_ipython
+ipython = get_ipython()
+
+# autoreload extension
+# if 'autoreload' not in ipython.extension_manager.loaded:
+#     get_ipython().run_line_magic('load_ext', 'autoreload')
+
+# get_ipython().run_line_magic('autoreload', '2')
+
+# Visualizations
+import seaborn as sns
+
+def BestCentroid(file_number, start=10, stop=50, step=1):
+    for v in file_number:
+        file_name = f"/home/ATLAS-T3/eferri/File/FrontendFileGroup/storm-frontend-202003{v}-mask-group.txt"
+        with open(f'/home/ATLAS-T3/eferri/File/DataSet/data-set-frontend-202003{v}.csv') as file_name:
+            X = np.loadtxt(file_name, delimiter=",")
+
+        labels = np.arange(0, stop)
+        Sum_of_squared_distances = []
+        silhouette_avg = []
+
+        K = range(start, stop, step)    
+
+        for n_clusters in K:
+            # run K-Means algorithm: 6 clusters
+            km = KMeans(n_clusters=n_clusters, 
+                        init='k-means++', 
+                        max_iter=500, 
+                        n_init=100,
+        #                 verbose=1
+                       )
+
+            print("Clustering sparse data with %s" % km)
+            t0 = time()
+            km.fit(X)
+            print("done in %0.3fs" % (time() - t0))
+            print()
+
+            print("We have {} centroids represented as {}-dimensional points.".format(km.cluster_centers_.shape[0],
+                                                                                      km.cluster_centers_.shape[1]))
+
+            Sum_of_squared_distances.append(km.inertia_)
+            cluster_labels = km.labels_
+            # silhouette score
+            silhouette_avg.append(silhouette_score(X, cluster_labels))
+
+            logs["kmean_labels"] = km.labels_
+
+            label = np.unique(km.labels_)
+            count = [Counter(km.labels_)[i] for i in label]
+
+        plt.plot(K,Sum_of_squared_distances,'bx-')
+        plt.xlabel('Values of K') 
+        plt.ylabel('Sum of squared distances/Inertia')
+        ax.set(xticks=np.arange(10, 50, 2))
+        plt.grid()
+        plt.title('Elbow Method For Optimal k')
+        plt.savefig(f'/home/ATLAS-T3/eferri/File/BestCentroid/frontend-202003{v}-squared distances', bbox_inches ="tight")
+        plt.show()
+
+        plt.plot(K,silhouette_avg, 'bx-')
+        plt.xlabel('Values of K') 
+        plt.ylabel('Silhouette score') 
+        ax.set(xticks=np.arange(10, 50, 2))
+        plt.grid()
+        plt.title('Silhouette analysis For Optimal k')
+        plt.savefig(f'/home/ATLAS-T3/eferri/File/BestCentroid/frontend-202003{v}-silhouette score', bbox_inches ="tight")
+        plt.show()
+
+        maxS = max(silhouette_avg)
+        centroid += [K[silhouette_avg.index(maxS)]]
+    
+    print('Mean number of centroid that maximize the silhouette score is', mean(centroid))
+    
+BestCentroid(['07','08','09','10','11','12','13'])
